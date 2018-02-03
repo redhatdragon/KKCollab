7a2b8d95d8d18b7e25c2f7b686db96db859ca696
redhatdragon <paulabrams99@yahoo.com>
2018-02-03 04:29:44 -0500

    Testing rax

--- Jinxicaya/Jinxicaya/main.cpp
+++ Jinxicaya/Jinxicaya/main.cpp
@@ -5,8 +5,15 @@ extern "C"{
 }
 
 int main(int argc, char** argv){
+    void *y = new char(5);
+
     rax *rt = raxNew();
-    raxInsert(rt,(unsigned char*)"mykey",5,0,NULL);
+    raxInsert(rt,(unsigned char*)"122>122>122>122>122",5,y,NULL);
+    raxInsert(rt,(unsigned char*)"142>141>141>141>141",5,0,NULL);
+    raxInsert(rt,(unsigned char*)"141>141>141>141>129",5,0,NULL);
+    raxInsert(rt,(unsigned char*)"100>133>141>141>141",5,0,NULL);
+    raxInsert(rt,(unsigned char*)"133>141>141>141>141",5,0,NULL);
+    raxShow(rt);
 
     return 0;
 }
