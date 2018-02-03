db353a2f68f573fc56bcc0c872604fca1d6cb29e
redhatdragon <paulabrams99@yahoo.com>
2018-02-03 07:06:32 -0500

    Added functions to feed files to rax

--- Jinxicaya/Jinxicaya/main.cpp
+++ Jinxicaya/Jinxicaya/main.cpp
@@ -1,19 +1,41 @@
+#include <iostream>
+#include <fstream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 extern "C"{
     #include "rax-master/rax.h"
 }
 
+using namespace std;
+
+rax *rt = raxNew();
+
+void addToTree(unsigned char *_str, int _length){
+    void *counter = new int(0);
+    if(raxInsert(rt,_str,_length,counter,NULL)){
+        counter = raxFind(rt,_str,_length);
+        counter++;
+    }
+}
+void addFileToTree(const char *fileName){
+    string line;
+    unsigned char line2[500];
+    ifstream myfile (fileName);
+    if(myfile.is_open()){
+        while ( getline (myfile,line) ){
+            unsigned char *str = (unsigned char *)line.c_str();
+            addToTree(str,line.length());
+        }
+        myfile.close();
+    }else{
+        cout << "Error: Can't read file!" << endl;
+    }
+}
+
 int main(int argc, char** argv){
-    void *y = new char(5);
 
-    rax *rt = raxNew();
-    raxInsert(rt,(unsigned char*)"122>122>122>122>122",5,y,NULL);
-    raxInsert(rt,(unsigned char*)"142>141>141>141>141",5,0,NULL);
-    raxInsert(rt,(unsigned char*)"141>141>141>141>129",5,0,NULL);
-    raxInsert(rt,(unsigned char*)"100>133>141>141>141",5,0,NULL);
-    raxInsert(rt,(unsigned char*)"133>141>141>141>141",5,0,NULL);
+    rt = raxNew();
+    addFileToTree("twilight0_patterns.txt");
     raxShow(rt);
-
     return 0;
 }
