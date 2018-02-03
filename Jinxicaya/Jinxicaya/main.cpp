6d154933220baf3022a39de68a4b1e6d7199e0b8
redhatdragon <paulabrams99@yahoo.com>
2018-02-03 03:44:27 -0500

    Fixed rax linking

--- Jinxicaya/Jinxicaya/main.cpp
+++ Jinxicaya/Jinxicaya/main.cpp
@@ -1,8 +1,12 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include "rax-master/rax.h"
+extern "C"{
+    #include "rax-master/rax.h"
+}
 
 int main(int argc, char** argv){
+    rax *rt = raxNew();
+    raxInsert(rt,(unsigned char*)"mykey",5,0,NULL);
 
     return 0;
 }
